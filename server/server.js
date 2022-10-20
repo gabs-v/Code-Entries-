a2b38a64d5baa8cc6657635fd120c3f9e7c317cb
gabs-v <gabyvazquez8297@gmail.com>
2022-10-19 20:57:34 -0700

    back end

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use;(express.json(),
+app.use(express.json(),
 express.urlencoded({extended: true}))
 
 
@@ -9,4 +9,4 @@ require('./config/mongoose.config');
 require('./routes/code.routes')(app)
 
 
-app.listen(8000, () => console.log("server running on port 8000! :)"))
+app.listen(8000, () => console.log("server running on port 8000!"))
